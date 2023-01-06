5b4462250dcbbb4cda0fb674c6789f4c0adab85a
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-01-06 18:37:58 -0300

    src folder commit fix

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+const users = [];
+const tweets = [];
+let userTweets = [];
+app.use(cors());
+app.use(express.json());
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send(tweet)
+})
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send(user)
+})
+
+function getTweets() {
+    userTweets = []
+    const targetTweets = tweets.slice(0).slice(-10) 
+    targetTweets.map((tweet) => {
+        const user = users.find((user) => user.username === tweet.username);
+        userTweets.unshift({ ...tweet, avatar: user.avatar });     
+    })
+}
+
+app.get("/tweets", (req, res) => {
+    getTweets()
+    res.send(userTweets);
+})
+
+
+
+
+
+app.listen(PORT);
