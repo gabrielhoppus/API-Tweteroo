ea373db497249f9181fd33775c46b09faaab7bb0
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-01-09 11:13:23 -0300

    refactor: pagination and headers

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 5000;
 const users = [];
 const tweets = [];
-let userTweets = [];
 app.use(cors());
 app.use(express.json());
 
@@ -13,45 +12,58 @@ app.post("/sign-up", (req, res) => {
     const user = req.body;
 
     if (!user.username || !user.avatar || typeof user.username !== 'string' || typeof user.avatar !== 'string') {
-        res.status(400).send("Todos os campos são obrigatórios!")
+        res.status(400).send("Todos os campos são obrigatórios!");
     }
     users.push(user);
-    res.status(201).send("OK")
+    res.status(201).send("OK");
 })
 
 app.post("/tweets", (req, res) => {
-    const tweet = req.body;
-    const findUser = users.find((user) => user.username === tweet.username);
+    const username = req.headers.username;
+    const text = req.body.tweet;
+    const tweet = { username, tweet: text };
+    const findUser = users.find((user) => user.username === username);
 
-    if (!tweet.username || !tweet.tweet || typeof tweet.tweet !== 'string') {
-        res.status(400).send("Todos os campos são obrigatórios!")
+    if (!tweet.tweet || typeof tweet.tweet !== 'string') {
+        res.status(400).send("Todos os campos são obrigatórios!");
     }
 
     !findUser ?
         res.status(401).send("UNAUTHORIZED") :
-        tweets.push(tweet)
-    res.status(201).send("CREATED")
+        tweets.push(tweet);
+    res.status(201).send("CREATED");
 })
 
-
-function getTweets() {
-    userTweets = []
-    const targetTweets = tweets.slice(0).slice(-10)
-    targetTweets.map((tweet) => {
-        const user = users.find((user) => user.username === tweet.username);
-        userTweets.unshift({ ...tweet, avatar: user.avatar });
-    })
-}
-
 app.get("/tweets", (req, res) => {
-    getTweets()
+    let targetTweets = [];
+    const { page } = req.query;
+    const username = req.headers.username;
+    const reversedTweets = [...tweets].reverse();
+
+    if (req.query.page) {
+        if (page < 1) {
+            res.status(400).send("Informe uma página válida!");
+            return;
+        }
+        const minSlice = (page - 1) * 10;
+        const maxSlice = page * 10;
+        targetTweets = reversedTweets.slice(minSlice, maxSlice);
+    } else {
+        targetTweets = reversedTweets.slice(0, 10);
+    }
+
+    targetTweets.map((tweet) => {
+        const user = users.find((user) => user.username === username);
+        userTweets.push({ ...tweet, avatar: user.avatar });
+    })
+
     res.send(userTweets);
 })
 
 app.get("/tweets/:username", (req, res) => {
-    const username = req.params.username
-    getTweets()
-    const usernameTweets = tweets.filter((tweet) => tweet.username === username)
+    const username = req.params.username;
+    const reversedTweets = [...tweets].reverse();
+    const usernameTweets = reversedTweets.filter((tweet) => tweet.username === username);
     res.send(usernameTweets);
 })
 
